1412105630f525efbde11f241ea6b831495ba20f
Andy Garcia <acgarcia@ucdavis.edu>
2020-10-16 11:39:33 -0700

    fixed reject button functionality

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,46 +6,59 @@ import './Game.css';
 
 const Game = ({ yelpResults }) => {  
   // Our state for managing our list of restaurant indexes
-  const [restaurantIndexes, setRestaurantIndexes] = useState([]);
+  const [restaurantList, setRestaurantList] = useState([]);
 
-  // This is the restaurant we want to show
-  const [currentRestaurantIndex, setCurrentRestaurantIndex] = useState(0);
+  // This is the current restaurant we are displaying
+  const [currentRestaurant, setCurrentRestaurant] = useState(4);
 
   // We are only running this at initial render and anytime
   // yelpResults gets updated (only once per food and location submit)
   useEffect(() => {
-    setRestaurantIndexes(Object.keys(yelpResults))
+    setRestaurantList(Object.keys(yelpResults))
   }, [yelpResults]);
 
   // Rerenders our component whenever these values change
-  useEffect(() => {}, [currentRestaurantIndex, restaurantIndexes])
+  useEffect(() => {}, [currentRestaurant, restaurantList])
 
   // Our function to get a random restaurant
   const randomRestaurant = () => {
-    let randomIndex = restaurantIndexes[Math.floor(Math.random() * restaurantIndexes.length)];
+    let randomIndex = restaurantList[Math.floor(Math.random() * restaurantList.length)];
 
     // Prevents us from getting the same restaurant in a row
-    while (randomIndex === currentRestaurantIndex) {
-      randomIndex = restaurantIndexes[Math.floor(Math.random() * restaurantIndexes.length)];
+    while (randomIndex === currentRestaurant) {
+      randomIndex = restaurantList[Math.floor(Math.random() * restaurantList.length)];
     }
-
+    console.log(`randomRest Index: ${randomIndex}`)
     return randomIndex;
   }
 
   // Function pertaining to clicking the reject button
   const handleRejectClick = () => {
-    console.log('Reject');
-    
-    // Turning Approve Route on and Reject route off 
-    // return restaurantCard;
+    // Removing the current restaurant index from our array
+    let updatedRestaurantList = restaurantList;
+    console.log(`Before Splice Updated Restaurant List: ${updatedRestaurantList}`)
+    updatedRestaurantList.splice(restaurantList.indexOf(currentRestaurant), 1);
+
+    // console.log(`Old Current Restaurant: ${currentRestaurant}`)
+    console.log(`Updated Restaurant List: ${updatedRestaurantList}`)
+    setRestaurantList(updatedRestaurantList);
+    // console.log(`Restaurant List: ${restaurantList}`)
+
+    // Display a new restaurant
+    // console.log(`current rest before set ${currentRestaurant}`);
+    // console.log(`Random Number in updated rest list ${updatedRestaurantList[Math.floor(Math.random() * updatedRestaurantList.length)]}`)
+    let newCurrentRestaurant = updatedRestaurantList[Math.floor(Math.random() * updatedRestaurantList.length)];
+    console.log(`Current Rest in Reject Fn: ${newCurrentRestaurant}`)
+    setCurrentRestaurant(newCurrentRestaurant);
+    // console.log(`current rest after set ${currentRestaurant}`);
+    // console.log(`New Current Restaurant: ${currentRestaurant}`)
   }  
 
   // Function pertaining to clicking the approve button
   const handleApproveClick = () => {
     // Setting a new restaurant to display
-    setCurrentRestaurantIndex(randomRestaurant());
-    console.log(currentRestaurantIndex);
-  }  
+    setCurrentRestaurant(randomRestaurant());
+  }
 
   // Our JSX that shows our restaurant
   let restaurantCard = (
@@ -53,8 +66,8 @@ const Game = ({ yelpResults }) => {
       <RejectButton handleRejectClick={handleRejectClick} />
       {/* Chooses a random restaurant to view in our remaining restaurants */}
       <Card 
-        yelpResults={yelpResults[currentRestaurantIndex]}
-        // yelpResults={yelpResults[restaurantIndexes[Math.floor(Math.random() * restaurantIndexes.length)]]}
+        yelpResults={yelpResults[currentRestaurant]}
+        // yelpResults={yelpResults[restaurantList[Math.floor(Math.random() * restaurantList.length)]]}
       />
       <ApproveButton handleApproveClick={handleApproveClick} />
     </div>
@@ -63,16 +76,29 @@ const Game = ({ yelpResults }) => {
   // Show Approve Route
   if (Object.keys(yelpResults).length === 0) {
     // console.log('Error Handling')
-    // console.log(`Restaurant Indexes: ${restaurantIndexes}`);
+    // console.log(`Restaurant Indexes: ${restaurantList}`);
     return (
       <div>
         Website was refreshed, you have to click "Home" and restart.
       </div>
     )
     // Our initial render, meaning no buttons were clicked and no error
+  } else if (restaurantList.length === 1) {
+    return (
+      <div>
+        <p>You'll be eating here :D!</p>
+        <Card 
+          yelpResults={yelpResults[currentRestaurant]}
+          // yelpResults={yelpResults[restaurantList[Math.floor(Math.random() * restaurantList.length)]]}
+        />        
+      </div>
+    )
   } else {
       // console.log('Initial Render')
-      // console.log(`Restaurant Indexes: ${restaurantIndexes}`);
+      // console.log(`Restaurant Indexes: ${restaurantList}`);
+      // console.log(`Restaurant Indexes Length: ${restaurantList.length}`);
+      // console.log(yelpResults);
+      console.log(`Current Restaurant Index: ${currentRestaurant}`)
       return restaurantCard; 
   }
 };
