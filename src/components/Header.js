e2fd339abf06697faf4021b60cdf0a06ace6219a
Andy Garcia <acgarzeah@gmail.com>
2020-12-07 16:48:07 -0800

    Revert "Revert "changed file structure""
    This reverts commit b67804da9792581a6987665c4c66e2efdbf5a2ba.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import Link from './Link';
+import React from "react";
+import Link from "./Link";
 
 const Header = () => {
   return (
-    <div className="ui large menu" style={{background: "#F26666", borderRadius: "0px"}}>
-      <div>  
+    <div
+      className="ui large menu"
+      style={{ background: "#F26666", borderRadius: "0px" }}
+    >
+      <div>
         <Link href="/" className="item" style={{ color: "white" }}>
           Home
         </Link>
       </div>
     </div>
-  )
+  );
 };
 
 export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,46 @@
-import React from 'react';
-import { useMediaQuery } from 'react-responsive';
-import Search from './Search';
-import Link from './Link';
-import './Home.css'
-import mobileLogo from '../assets/images/mobileLogo.png';
-import desktopLogo from '../assets/images/desktopLogo.png';
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+import Search from "./Search";
+import Link from "./Link";
+import "./Home.css";
+import mobileLogo from "../assets/images/mobileLogo.png";
+import desktopLogo from "../assets/images/desktopLogo.png";
 
 const Home = ({ handleFoodSearch, handleLocationSearch }) => {
-  const isMobile = useMediaQuery({ query: '(max-device-width: 767.98px)' });
+  const isMobile = useMediaQuery({ query: "(max-device-width: 767.98px)" });
   let logo = null;
 
   if (isMobile) {
-    logo = 
+    logo = (
       <div className="logo-container">
         <img
-          style={{maxWidth: "90%"}}
+          style={{ maxWidth: "90%" }}
           src={mobileLogo}
           alt="Restaurant Tinder Icon"
         />
-      </div>     
+      </div>
+    );
   } else {
-    logo = 
+    logo = (
       <div className="logo-container">
-        <img
-          src={desktopLogo}
-          alt="Restaurant Tinder Icon"
-        />
-      </div>       
+        <img src={desktopLogo} alt="Restaurant Tinder Icon" />
+      </div>
+    );
   }
 
   return (
     <div className="home-container">
       {logo}
-      <Search 
-          text={"What are you craving?"}
-          onSelectTerm={handleFoodSearch}
-        />
-      <Search 
+      <Search text={"What are you craving?"} onSelectTerm={handleFoodSearch} />
+      <Search
         text={"Enter City and State"}
         onSelectTerm={handleLocationSearch}
       />
-      <Link
-        href="/loading"
-        className="fluid ui button submit-button"
-        >
+      <Link href="/loading" className="fluid ui button submit-button">
         Submit
       </Link>
     </div>
-  )
+  );
 };
 
 export default Home;
