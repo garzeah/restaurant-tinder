32c5c96448643a5dcc232080281f48f09946d05a
Andy Garcia <acgarzeah@gmail.com>
2020-12-11 10:42:26 -0800

    Revert "testing proxy url"
    This reverts commit 6dbb64b372804b74b170b3c5c97e7ec1069b1928.

--- src/apis/yelp-fusion.js
+++ src/apis/yelp-fusion.js
@@ -4,15 +4,12 @@ import axios from "axios";
 const KEY =
   "qXKScxeUYgpGRRrGLf3fC_UCbSQMsczdCBqMKjM55IkjPQ0bkbhynb58TnRJFXLd0oWv8lRULvCp6VJ-ItTxFyPkzN6_qHSAUWCr8zT340Eput70UoPRHyyPAkxxX3Yx";
 
-// Proxy
-const proxyURL = "https://cors-anywhere.herokuapp.com/";
-
 // Our Yelp Fusion code that sends a GET request
 export default axios.create({
-  baseURL: proxyURL + "https://api.yelp.com/v3",
+  baseURL: `${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3`,
   headers: {
     Authorization: `Bearer ${KEY}`,
-    // "Access-Control-Allow-Origin": "*",
-    // crossorigin: true,
+    "Access-Control-Allow-Origin": "*",
+    crossorigin: true,
   },
 });
