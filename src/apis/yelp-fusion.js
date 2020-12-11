21ec3ccaa1eec13645ac74238b3e86a29e67539b
Andy Garcia <acgarzeah@gmail.com>
2020-12-11 09:32:59 -0800

    updated cors stuff

--- src/apis/yelp-fusion.js
+++ src/apis/yelp-fusion.js
@@ -1,12 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Yelp Fusion authentication
-const KEY = 'qXKScxeUYgpGRRrGLf3fC_UCbSQMsczdCBqMKjM55IkjPQ0bkbhynb58TnRJFXLd0oWv8lRULvCp6VJ-ItTxFyPkzN6_qHSAUWCr8zT340Eput70UoPRHyyPAkxxX3Yx';
+const KEY =
+  "qXKScxeUYgpGRRrGLf3fC_UCbSQMsczdCBqMKjM55IkjPQ0bkbhynb58TnRJFXLd0oWv8lRULvCp6VJ-ItTxFyPkzN6_qHSAUWCr8zT340Eput70UoPRHyyPAkxxX3Yx";
 
 // Our Yelp Fusion code that sends a GET request
 export default axios.create({
-  baseURL: `${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3`,
+  baseURL: `${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3`,
   headers: {
-    Authorization: `Bearer ${KEY}`
+    Authorization: `Bearer ${KEY}`,
+    "Acess-Control-Allow-Origin": "*",
   },
-})
+});
