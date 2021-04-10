756bb02024ae90fa8b82f221d7fe2bd1518d0619
Andy Garcia <acgarzeah@gmail.com>
2021-04-10 01:11:43 -0700

    updated restaurant  api

--- src/apis/yelp-fusion.js
+++ src/apis/yelp-fusion.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 // Yelp Fusion authentication
 const KEY =
-  "qXKScxeUYgpGRRrGLf3fC_UCbSQMsczdCBqMKjM55IkjPQ0bkbhynb58TnRJFXLd0oWv8lRULvCp6VJ-ItTxFyPkzN6_qHSAUWCr8zT340Eput70UoPRHyyPAkxxX3Yx";
+	"P0ijk2yDNq50z9A6wcVkf1JbYxs8FGrGbK60_wMvOL1N8CyWm0hxq5tCV00S5Gidt7HBWK695UxADs47ETs7knV6kXMjM_4XFdN8sIMoI3-HbmtWXNh0Df-OjApxYHYx";
 
 // Our Yelp Fusion code that sends a GET request
 export default axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3",
-  headers: {
-    Authorization: `Bearer ${KEY}`,
-  },
+	baseURL: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3",
+	headers: {
+		Authorization: `Bearer ${KEY}`
+	}
 });
